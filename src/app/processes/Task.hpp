76bfc65709af371576257b79a601ae4cb2e0dace
Ansh Chughm <anshchugh20@gmail.com>
2025-01-02 19:52:03 +0000

    [A:1] fixed inline ternary check with if else block to fix the program [Now completely works]

--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -4,41 +4,59 @@
 #include <iostream>
 #include <sstream>
 #include "../fileHandling/IO.hpp"
-enum class Action{
+enum class Action
+{
     ENCRYPT,
     DECRYPT
 };
 
-struct Task{
+struct Task
+{
     std::string file_path;
     std::fstream f_stream;
     Action action;
-    Task(std::fstream &&stream, Action act, std::string file_path): f_stream(std::move(stream)), file_path(file_path), action(act){};
+    Task(std::fstream &&stream, Action act, std::string file_path) : f_stream(std::move(stream)), file_path(file_path), action(act) {};
 
-    std::string toString(){
+    std::string toString()
+    {
         std::ostringstream oss;
-        oss << file_path<<"," << (action==Action::ENCRYPT) ? "Encrypt" : "Decrypt";
+
+        oss << file_path << ",";
+        if (action == Action::ENCRYPT)
+        {
+            oss << "ENCRYPT";
+        }
+        else
+        {
+            oss << "DECRYPT";
+        }
         // test.txt Encrypt f_stream
         return oss.str();
     }
-    static Task fromString(const std::string &taskData){
+    static Task fromString(const std::string &taskData)
+    {
         std::istringstream iss(taskData);
         std::string filePath;
         std::string actionStr;
-        if(std::getline(iss, filePath, ',') && std::getline(iss,actionStr)){
+        if (std::getline(iss, filePath, ',') && std::getline(iss, actionStr))
+        {
             Action action = actionStr == "ENCRYPT" ? Action::ENCRYPT : Action::DECRYPT;
             IO io(filePath);
             std::fstream f_stream = std::move(io.getFileStream());
-            if(f_stream.is_open()){
-                return Task(std::move(f_stream),action, filePath);
-            }else{
+            if (f_stream.is_open())
+            {
+                return Task(std::move(f_stream), action, filePath);
+            }
+            else
+            {
                 throw std::runtime_error("Failed to open file: " + filePath);
             }
-        }else{
+        }
+        else
+        {
             throw std::runtime_error("Error in Task data: " + taskData);
         }
     }
 };
-
 
 #endif
